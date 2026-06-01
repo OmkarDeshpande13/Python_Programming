216325a85ceded957c3384be9e8a142353789e29
OmkarDeshpande13 <omkardeshpande59@gmail.com>
2026-06-01 21:29:13 +0530

    Lambda function8

---
+++ Python_Assignments/Assignment4/program8.py
@@ -0,0 +1,14 @@
+Addition = lambda x,y : x + y
+
+
+Value1 = int(input("Enter First Number:"))
+Value2 = int(input("Enter Second Number:"))
+
+
+iRet = 0
+
+iRet = Addition(Value1,Value2)
+
+print("Addition is:",iRet)
+
+
