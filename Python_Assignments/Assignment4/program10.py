d7439931775122af8a124c2cec35fff3a566ea52
OmkarDeshpande13 <omkardeshpande59@gmail.com>
2026-06-01 21:29:13 +0530

    Lambda function10

---
+++ Python_Assignments/Assignment4/program10.py
@@ -0,0 +1,16 @@
+Check_Largest = lambda x,y,z : max(x,y,z)
+
+
+Value1 = int(input("Enter First Number:"))
+Value2 = int(input("Enter Second Number:"))
+Value3 = int(input("Enter third Number:"))
+
+
+
+iRet = 0
+
+iRet = Check_Largest(Value1,Value2,Value3)
+
+print("Largest is:",iRet)
+
+
