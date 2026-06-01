976b3cbfc4cece9210fff7a0cd450f0e844258e5
OmkarDeshpande13 <omkardeshpande59@gmail.com>
2026-06-01 21:29:13 +0530

    Lambda function9

---
+++ Python_Assignments/Assignment4/program9.py
@@ -0,0 +1,14 @@
+Multiplication = lambda x,y : x * y
+
+
+Value1 = int(input("Enter First Number:"))
+Value2 = int(input("Enter Second Number:"))
+
+
+iRet = 0
+
+iRet = Multiplication(Value1,Value2)
+
+print("Multiplication is:",iRet)
+
+
