0c1779a8ba2caf2b6077d2899775400fd74140dd
OmkarDeshpande13 <omkardeshpande59@gmail.com>
2026-06-10 00:32:28 +0530

    Display Numbers in Reverse Order

---
+++ Python_Assignments/Assignment6/program5.py
@@ -0,0 +1,18 @@
+"""
+def Display():
+    i = 11
+    while(i != 1):
+        i = i -1
+        print(i)
+"""
+def Display():
+
+    for i in range(10,0,-1):
+        print(i)
+def main():
+    
+     Display()
+    
+    
+if __name__ == "__main__":
+    main()
