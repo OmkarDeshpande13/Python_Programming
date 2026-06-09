9c3f2f5502be9c4029d74ef54dc5374f1c494866
OmkarDeshpande13 <omkardeshpande59@gmail.com>
2026-06-10 00:34:44 +0530

    To Display Pattern

---
+++ Python_Assignments/Assignment6/program8.py
@@ -0,0 +1,16 @@
+
+def Pattern(No):
+
+    for i in range(No):
+        print("*")
+
+    
+
+def main():
+
+    Value = int(input("Enter the Number:"))
+
+    Pattern(Value)
+
+if __name__ == "__main__":
+    main()
