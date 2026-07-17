70d04579038ddbcaabffad2aeb63a7f3c2b5eeaa
OmkarDeshpande13 <omkardeshpande59@gmail.com>
2026-07-17 23:38:21 +0530

    Pattern Printing

---
+++ Python_Assignments/Assignment7/program6.py
@@ -0,0 +1,21 @@
+                    
+def Display(row,col):
+
+    for i in range(row,0,-1):
+
+        for j in range(1,i+1):
+            print("* ",end=" ")
+
+        print("\r")
+
+
+
+def main():
+
+    rows =  int(input("Enter the Numbe rows : "))
+    cols =  int(input("Enter the Number columns : "))
+
+    Display(rows,cols)
+
+if __name__ == "__main__":
+    main()
